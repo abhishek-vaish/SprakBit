5dc272436b6c7d26dc16b217aadb94a3ea898b0b
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-06-17 06:49:05 +0530

    SecurityInitializer class created

---
+++ src/main/java/com/social/app/security/SecurityInitializer.java
@@ -0,0 +1,13 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 17-06-2020
+ *   Time: 03:06 AM
+ */
+
+package com.social.app.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
