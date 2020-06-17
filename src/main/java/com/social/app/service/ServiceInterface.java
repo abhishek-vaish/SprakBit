43559938af385aadaccbc840c54785ed27794326
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-06-17 06:46:10 +0530

    ServiceInterface interface created

---
+++ src/main/java/com/social/app/service/ServiceInterface.java
@@ -0,0 +1,24 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 11-06-2020
+ *   Time: 05:26 AM
+ */
+
+
+package com.social.app.service;
+
+import com.social.app.model.RecordsAsPurchase;
+
+import java.util.List;
+
+public interface ServiceInterface {
+
+    void saveUser(RecordsAsPurchase recordsAsPurchase);
+
+    List<RecordsAsPurchase> getUser();
+
+    void delete(int id);
+
+    List<RecordsAsPurchase> search(String firstName);
+}
