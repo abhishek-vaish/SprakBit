0259b45e415eae4ea45727b103828d0816b07b2d
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-06-17 06:40:43 +0530

    Initial commit

---
+++ src/test/java/com/social/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
