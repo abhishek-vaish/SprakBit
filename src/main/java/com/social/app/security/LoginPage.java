4c449bc80e1da15daeef6a8414be3ff0df2c6988
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-06-17 07:25:38 +0530

    LoginPage completed

--- src/main/java/com/social/app/security/LoginPage.java
+++ src/main/java/com/social/app/security/LoginPage.java
@@ -10,10 +10,8 @@ package com.social.app.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
@@ -22,13 +20,17 @@ public class LoginPage extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .passwordEncoder(new BCryptPasswordEncoder()).withUser("demo").password("$2a$10$2WqauZuyNa48YHU/88c16uHHXT8.2KtcRrjffPHGqpxkmfSG60HQm").roles("ADMIN");
+                .withUser("demo")
+                .password("$2a$10$SxNTPLZ49I61aa/eizQUM.4im7gQ3FidnH7ZTRlUJGTZzNKN98qdS")
+                .roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/users")
+                .hasRole("ADMIN")
+                .antMatchers("/api/**")
                 .hasRole("ADMIN")
                 .and()
                 .formLogin()
